2c6a99a4c65e4c5dfcf338c7beee2fcf6ab215e7
Gabriel Henrique Muller de Medeiros <gabriel_henrique2805@hotmail.com>
2023-11-30 18:35:52 -0300

    Adicionando o CORS

--- src/main/java/desenvolviomento/web/dev/perguntar/controller/PerguntaController.java
+++ src/main/java/desenvolviomento/web/dev/perguntar/controller/PerguntaController.java
@@ -6,6 +6,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import desenvolviomento.web.dev.perguntar.service.UsuarioService;
 
 @RestController
 @RequestMapping(path="/pergunta")
+@CrossOrigin(origins = {"http://localhost:4200","http://localhost:5500"}, maxAge = 3600)
 public class PerguntaController {
     @Autowired
     private PerguntaService perguntaService;
