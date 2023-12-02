c557f30591e90e420a9ae67c1927d8ab831fe542
Gabriel Henrique Muller de Medeiros <gabriel_henrique2805@hotmail.com>
2023-12-02 09:52:20 -0300

    bug fix

--- src/main/java/desenvolviomento/web/dev/perguntar/model/dto/RespostaDTO.java
+++ src/main/java/desenvolviomento/web/dev/perguntar/model/dto/RespostaDTO.java
@@ -4,12 +4,59 @@ import lombok.Getter;
 import lombok.Setter;
 
 public class RespostaDTO {
-    @Getter
-    @Setter
+
     private Long id;
     private String conteudo;
     private Boolean solucao;
     private Long idPergunta;
     private Long idUsuario;
     private Long idRespostaPai;
+
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public void setSolucao(Boolean solucao) {
+        this.solucao = solucao;
+    }
+
+    public void setIdPergunta(Long idPergunta) {
+        this.idPergunta = idPergunta;
+    }
+
+    public void setIdUsuario(Long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public void setIdRespostaPai(Long idRespostaPai) {
+        this.idRespostaPai = idRespostaPai;
+    }
+
+    public Boolean getSolucao() {
+        return solucao;
+    }
+
+    public Long getIdPergunta() {
+        return idPergunta;
+    }
+
+    public Long getIdUsuario() {
+        return idUsuario;
+    }
+
+    public Long getIdRespostaPai() {
+        return idRespostaPai;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
